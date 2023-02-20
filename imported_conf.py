3096a38f47d1a7c7b3a31a1c5f8dc2264f1a3d79
Anonymous <ilvi142014@gmail.com>
2023-02-20 14:09:17 +0200

    Update imported_conf.py

--- imported_conf.py
+++ imported_conf.py
@@ -1,4 +1,48 @@
-(self):
+import psycopg2
+
+
+class Notifier:
+    def init(self, dbname, user, password, host, port):
+        self.conn = psycopg2.connect(
+            dbname=dbname,
+            user=user,
+            password=password,
+            host=host,
+            port=port
+        )
+
+    def set(self, row_id, column, value):
+        with self.conn.cursor() as cur:
+            cur.execute(
+                f"UPDATE notifier SET {column} = %s WHERE row_id = %s",
+                (value, row_id)
+            )
+        self.conn.commit()
+
+    def get(self):
+        with self.conn.cursor() as cur:
+            cur.execute("SELECT * FROM notifier")
+            columns = [desc[0] for desc in cur.description]
+            rows = cur.fetchall()
+            data = []
+            for row in rows:
+                data.append(dict(zip(columns, row)))
+        return data
+
+    def new(self, row_id, phone, tattoo_date, notif_medic_every_hours, notif_master_every_days, last_medic_notif, last_master_notif, status):
+        with self.conn.cursor() as cur:
+            cur.execute(
+                "INSERT INTO notifier (row_id, phone, tattoo_date, notif_medic_every_hours, notif_master_every_days, last_medic_notif, last_master_notif, status) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)",
+                (row_id, phone, tattoo_date, notif_medic_every_hours, notif_master_every_days, last_medic_notif, last_master_notif, status)
+            )
+        self.conn.commit()
+
+    def remove(self, row_id):
+        with self.conn.cursor() as cur:
+            cur.execute("DELETE FROM notifier WHERE row_id = %s", (row_id,))
+        self.conn.commit()
+
+    def __del__(self):
         self.conn.close()
 
 
